cc5464951e784db9b9d6eebf5a9c5d417636f939
Jacquelline Beltran <jacquelline500@gmail.com>
2019-04-13 03:44:34 -0700

    Format signup

--- client/src/containers/components/signup.js
+++ client/src/containers/components/signup.js
@@ -1,43 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import styled from "styled-components";
 
-const signup_element= (
-  <form>
-    <label>
-      fullname
-    </label>
-    <input type="text" placeholder='fullname'/>
-    <label>
-      email
-    </label>
-    <input type="text" placeholder='email'/>
-    <label>
-      birthday
-    </label>
-    <input type="text" placeholder='birthday'/>
-    <label>
-      address
-    </label>
-    <input type="text" placeholder='address'/>
-    <label>
-      password
-    </label>
-    <input type="text" placeholder='password'/>
-    <label>
-      verify password
-    </label>
-    <input type="text" placeholder='verify password'/>
-  
-    <input type="submit"/>
-    
-  </form>
+const Form = styled.form`
+  margin: 35% auto;
+  width: 50%;
+  font-size: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  input {
+    margin: 0 auto;
+    text-decoration: underline;
+    line-height: 2rem;
+  }
+`;
+
+const signup_element = (
+  <Form>
+    <label>Full name</label>
+    <input type="text" placeholder="fullname" />
+    <label>Email</label>
+    <input type="text" placeholder="email" />
+    <label>Birthday</label>
+    <input type="text" placeholder="birthday" />
+    <label>Address</label>
+    <input type="text" placeholder="address" />
+    <label>Password</label>
+    <input type="text" placeholder="password" />
+    <label>Verify Password</label>
+    <input type="text" placeholder="verify password" />
+
+    <input type="submit" />
+  </Form>
 );
-
 
 class SignUp extends Component {
   render() {
-    return (
-      signup_element
-    );
+    return signup_element;
   }
 }
 
