dbb51317452c6434dc640c2a37b8ff23526bb5eb
jonathan rafael lopez <socialme856@gmail.com>
2019-04-13 02:42:02 -0700

    starting login feature

---
+++ client/src/login.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+
+const loginElement = (
+  <form>
+    <input type="text" placeholder=''/>
+    
+  </form>
+);
+class Login extends Component {
+  render() {
+    return (
+      loginElement
+    );
+  }
+}
+
+export default App;
