d72481fcefaed7c22f1bd3300a25391a4dd899b1
jonathan rafael lopez <socialme856@gmail.com>
2019-04-12 23:57:21 -0700

    adding signin branch this is where signin will be worked at

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mapsRouter = require('./routes/maps');
 
 var app = express();
 
@@ -16,7 +17,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
@@ -29,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// start map routes here.
+app.use('/maps', mapsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
