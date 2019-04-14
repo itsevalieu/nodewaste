9af0bfc27f83018958a6aa9af20dc2f12c68cbad
itsevalieu <itsevalieu@gmail.com>
2019-04-14 05:15:35 -0700

    Adding done page

--- client/src/containers/components/Settings/Settings.jsx
+++ client/src/containers/components/Settings/Settings.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './Settings.css';
+import can from './done.png';
 
 class Settings extends Component {
   render() {
     return (
-        <div>
-            Settings
-        </div>
+      <div class='done'>
+        <img src={can} alt='can'/>
+        <p>Thank you for your contribution!</p>
+        <button className='donebutton'>Done</button>
+      </div>
     );
   }
 }
