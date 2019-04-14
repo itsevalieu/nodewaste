36168cdc9cacae7550af739e2246bb7ea6c052b5
itsevalieu <itsevalieu@gmail.com>
2019-04-13 18:46:26 -0700

    Add current location icon position

--- client/src/containers/components/Map/Map.jsx
+++ client/src/containers/components/Map/Map.jsx
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import {  GoogleMap, Marker, withGoogleMap, withScriptjs, InfoWindow } from 'react-google-maps'
 
 class Map extends Component {
+  state = {
+    currentPosition: {
+      lat: 34.023554, 
+      lng: -118.483395
+    }
+  }
+
   render() {
     return(
     <div>
@@ -16,6 +23,10 @@ class Map extends Component {
          >{marker.isOpen && <InfoWindow onCloseClick={()=>this.props.onToggle(marker)}><div>{marker.name}</div></InfoWindow>}
        </Marker>
       ))}
+      <Marker
+        icon="https://www.robotwoods.com/dev/misc/bluecircle.png"
+        position={this.state.currentPosition}
+      />
       </GoogleMap>
     </div>
     )
