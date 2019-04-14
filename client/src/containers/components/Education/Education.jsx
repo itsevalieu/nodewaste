ea81d00588afe95ebad15f83bfff86a102814754
itsevalieu <itsevalieu@gmail.com>
2019-04-14 05:03:22 -0700

    Adding scan page

--- client/src/containers/components/Education/Education.jsx
+++ client/src/containers/components/Education/Education.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './Education.css';
+import scanQR from './scan-qr.png';
 
 class Education extends Component {
   render() {
     return (
-        <div>
-            Education
+        <div class='scanqr'>
+            <img src={scanQR} alt='scancode'/>
+            <p>Line up the container QR code to scan it with your device's camera.</p>
         </div>
     );
   }
